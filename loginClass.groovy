848eedb7a23e092335333f639d153a925e585125
Lorenz Weiß <lorenz.weiss@sce.valantic.com>
2023-06-09 18:51:33 +0200

    fist wokring version

--- loginClass.groovy
+++ loginClass.groovy
@@ -4,6 +4,8 @@ import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 import org.jsoup.HttpStatusException
+import org.jsoup.Connection
+import org.jsoup.helper.HttpConnection
 
 class Login {
     static callLoginPage(){
@@ -11,17 +13,16 @@ class Login {
 
         def headers = [
             "authority": "sap-ain-azure-vhqbm59d.authentication.eu20.hana.ondemand.com",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "Accept-Language": "en",
-            "Cache-Control": "no-cache",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-language": "en",
+            "cache-control": "no-cache",
             "pragma": "no-cache",
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode": "navigate",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
             "sec-fetch-site": "none",
-            "Sec-Fetch-Site": "same-origin",
-            "Sec-Fetch-User": "?1",
-            "Upgrade-Insecure-Requests": "1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
             "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
             "sec-ch-ua-mobile": "?0",
             "sec-ch-ua-platform": "\"Windows\"",
@@ -52,7 +53,7 @@ class Login {
             nonce = nonce.split('=')[1]
         }
 
-        Map<String, String> cookies = loginResponse.cookies();
+        def cookies = loginResponse.cookies();
 
         return [state, nonce, cookies]
     }
@@ -60,10 +61,27 @@ class Login {
     static callOAuthAuthorize(state, nonce, cookies) {
         def url = "https://awx6f6jot.accounts.ondemand.com/oauth2/authorize?client_id=848f48cf-94fd-46a7-9faf-2f108036409d&response_type=code&redirect_uri=https%3A%2F%2Fsap-ain-azure-vhqbm59d.authentication.eu20.hana.ondemand.com%2Flogin%2Fcallback%2Fsap.custom&state=${state}&scope=openid+email&nonce=${nonce}"
 
-        println("cookies: " + cookies)
+        def headers = [
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Accept-Language": "en",
+            "Cache-Control": "no-cache",
+            "Pragma": "no-cache",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "same-site",
+            "Referer": "https://sap-ain-azure-vhqbm59d.authentication.eu20.hana.ondemand.com/",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "Host": "awx6f6jot.accounts.ondemand.com",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive"
+        ]
 
         def response = Jsoup.connect(url)
-                              .cookies(cookies)
+                              .headers(headers)
                               .execute()
 
         def html = response.parse().html()
@@ -73,10 +91,12 @@ class Login {
         def xsrfProtectionField = doc.select("#hidden-xsrfProtection-field").attr("value")
         def jsessionId = response.cookie("JSESSIONID")
 
-        return [csrfToken, xsrfProtectionField, jsessionId]
+        def cookies_response = response.cookies();
+
+        return [csrfToken, xsrfProtectionField, jsessionId, cookies_response]
     }
 
-    static postLogin(authenticityToken, xsrfProtection, state, nonce, jsessionId) {
+    static postLogin(authenticityToken, xsrfProtection, state, nonce, jsessionId, cookieauth) {
 
         println("state: " + state)
         println("nonce: " + nonce)
@@ -109,14 +129,7 @@ class Login {
             "css": "",
             "passwordlessAuthnSelected": "",
             "j_username": "Lorenz.weiss@sce.valantic.com",
-            "j_password": "M4!@X9dThAqz",
-        ]
-
-        def test = "\"${xsrfProtection}\""
-
-        def cookies = [
-            "JSESSIONID": jsessionId,
-            "XSRF_COOKIE": test.toString()
+            "j_password": "M4!@X9dThAqz"
         ]
 
         def headers = [
@@ -140,17 +153,60 @@ class Login {
             "Accept-Encoding": "gzip, deflate, br"
         ]
 
-        println("treeeeeeestststst");
-
         def requestBody = bodyParams.collect { k, v -> "${URLEncoder.encode(k, "UTF-8")}=${URLEncoder.encode(v, "UTF-8")}" }.join("&")
-        println(requestBody);
 
         try {
             def response = Jsoup.connect(url)
                     .requestBody(requestBody)
-                    .cookies(cookies)
+                    .followRedirects(false)
+                    .cookies(cookieauth)
                     .headers(headers)
                     .method(org.jsoup.Connection.Method.POST)
+                    .execute()
+
+            return response
+
+        } catch (Exception e) {
+            e.printStackTrace()
+
+        }
+    }
+
+    static loginCallback(response_from_post_login, cookieslogin) {
+
+        def url = response_from_post_login.header("Location")
+
+        Map<String, String> newMap = new HashMap<String, String>()
+        newMap.put("JSESSIONID", cookieslogin.get("JSESSIONID"))
+        newMap.put("__VCAP_ID__", cookieslogin.get("__VCAP_ID__"))
+        newMap.put("X-Uaa-Csrf", cookieslogin.get("X-Uaa-Csrf"))
+
+        def headers = [
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Accept-Language": "en",
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Origin": "https://awx6f6jot.accounts.ondemand.com",
+            "Pragma": "no-cache",
+            "Referer": "https://awx6f6jot.accounts.ondemand.com/saml2/idp/sso",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-User": "?1",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Postman-Token": "40c56f9a-1e29-4caa-939f-d8cbcbe7a084"
+        ]
+
+        try {
+            def response = Jsoup.connect(url)
+                    .cookies(newMap)
+                    .headers(headers)
                     .execute()
 
             return response
@@ -159,18 +215,9 @@ class Login {
             println(e.getMessage())
             e.printStackTrace()
 
-            println("Fehler beim Ausführen des Requests:")
-            println("URL: " + url)
-            println("Request-Methode: " + org.jsoup.Connection.Method.POST)
-            println("Request-Body: " + bodyParams)
-            println("Cookies: " + cookies)
-            println("Headers: " + headers)
         }
     }
 }
-
-// System.setProperty("http.proxyHost", "localhost")
-// System.setProperty("http.proxyPort", "8888")
 
 System.setProperty("https.proxyHost", "localhost")
 System.setProperty("https.proxyPort", "8866")
@@ -178,12 +225,15 @@ System.setProperty("https.proxyPort", "8866")
 def state_noce_cookie = Login.callLoginPage()
 def state = state_noce_cookie[0]
 def nonce = state_noce_cookie[1]
-def cookies = state_noce_cookie[2]
+def cookieslogin = state_noce_cookie[2]
 
-def xsrfProtection_csrfToken_jsessionId = Login.callOAuthAuthorize(state, nonce, cookies)
+def xsrfProtection_csrfToken_jsessionId = Login.callOAuthAuthorize(state, nonce, cookieslogin)
 def csrfToken = xsrfProtection_csrfToken_jsessionId[0]
 def xsrfProtection = xsrfProtection_csrfToken_jsessionId[1]
 def jsessionId = xsrfProtection_csrfToken_jsessionId[2]
+def cookieauth = xsrfProtection_csrfToken_jsessionId[3]
 
-def response = Login.postLogin(csrfToken, xsrfProtection, state, nonce, jsessionId)
-println(response.parse().html())
+def response = Login.postLogin(csrfToken, xsrfProtection, state, nonce, jsessionId, cookieauth)
+def response_login_callback = Login.loginCallback(response, cookieslogin)
+
+println(response_login_callback.parse().html())
